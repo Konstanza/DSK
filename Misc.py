0adf47ece9a74d90d29ca35d9ebeb0e080b51d50
Konstanza <Konstanza@users.noreply.github.com>
2017-03-20 00:32:39 -0400

    New: Portals

--- Misc.py
+++ Misc.py
@@ -11,6 +11,8 @@ import pygame
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
+YELLOW = (255,255,0)
+MAGENTA = (255,0,255)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 
@@ -24,7 +26,7 @@ IMAGE_PATH = os.path.join(dirname(__file__), "Images")
 backgrounds = [pygame.image.load(os.path.join(IMAGE_PATH, 'space-1.png')), pygame.image.load(os.path.join(IMAGE_PATH, 'space-2.png'))]
 blueShip = pygame.image.load(os.path.join(IMAGE_PATH, 'playerShip1_blue.png'))
 redShip = pygame.image.load(os.path.join(IMAGE_PATH, 'playerShip1_red.png'))
-portal = pygame.image.load(os.path.join(IMAGE_PATH, 'black_hole.png'))
+portalImage = pygame.image.load(os.path.join(IMAGE_PATH, 'black_hole.png'))
 bluShipRect = blueShip.get_rect()
 
 basicShot =  pygame.image.load(os.path.join(IMAGE_PATH, 'laserBlue04.png'))
